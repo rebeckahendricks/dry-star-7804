becae324074a18aea1e3eec9f3f1a89aa72540c9
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-09-26 10:37:47 -0600

    Create model tests for adult_patients and sort_by_name methods

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -10,4 +10,26 @@ RSpec.describe Patient do
     it {should validate_presence_of :name}
     it {should validate_presence_of :age}
   end
+
+  describe 'class methods' do
+    before :each do
+      @patient1 = Patient.create!(name: "Katie Bryce", age: 15)
+      @patient2 = Patient.create!(name: "Jeremiah Tate", age: 50)
+      @patient3 = Patient.create!(name: "Bonnie Crasnoff", age: 40)
+      @patient4 = Patient.create!(name: "George O'Malley", age: 32)
+      @patient5 = Patient.create!(name: "Maya Bishop", age: 18)
+    end
+
+    describe 'adult_patients' do
+      it 'can list only patients over the age of 18' do
+        expect(Patient.adult_patients).to eq([@patient2, @patient3, @patient4])
+      end
+    end
+
+    describe 'sort_by_name' do
+      it 'can sort patients by name alphabetically' do
+        expect(Patient.sort_by_name).to eq([@patient3, @patient4, @patient2, @patient1, @patient5])
+      end
+    end
+  end
 end
