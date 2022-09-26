daf9da0647bf1fcd7dae117ca7b0cfede735a9f3
Rebecka Hendricks <rebecka.hendricks@gmail.com>
2022-09-26 11:33:41 -0600

    Create patient_count and order_by_patient count methods in Doctor model & view for hospital show

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -7,4 +7,15 @@ class Doctor < ApplicationRecord
   validates :specialty, presence: true
   validates :university, presence: true
   validates :hospital_id, presence: true
+
+  def patient_count
+    patients.count
+  end
+
+  def self.order_by_patient_count
+    joins(:patient_doctors)
+    .select("doctors.*, count(patient_doctors) as patient_count")
+    .group('doctors.id')
+    .order('patient_count desc')
+  end
 end
